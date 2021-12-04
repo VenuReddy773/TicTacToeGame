a410f6b4042827900ccb2bf4620a8e00be29fbfc
“VenuReddy773” <venu706@gmail.com>
2021-12-04 09:34:56 +0530

    [VenuGopalReddy] Added: Tic Tac Toe Game Board

---
+++ TicTacToe/TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class TicTacToeGame
+    {
+        public void Board()
+        {
+            char[] arr = new char[10];
+            foreach(var item in arr)
+            {
+                arr[item] = ' ';
+            }
+        }
+    }
+}
